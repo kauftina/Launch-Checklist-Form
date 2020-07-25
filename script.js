0eade90b35d87fe35bc50e58c9db9392927ed5de
Tina <kauftina@aol.com>
2020-07-24 22:47:23 -0500

    finished

--- script.js
+++ script.js
@@ -1,13 +1,113 @@
 // Write your JavaScript code here!
+fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+            let index = 0;
+            response.json().then(function(json) {
+               let formSubmit = document.getElementById("formSubmit");
+              
+               formSubmit.addEventListener("click", function(){
+                  let missionTarget = document.getElementById("missionTarget");
+                  missionTarget.innerHTML = `
+                  <div>
+                     <h2>Mission Destination</h2>
+                     <ol>
+                        <li>Name: ${json[index].name}</li>
+                        <li>Diameter: ${json[index].diameter}</li>
+                        <li>Star: ${json[index].star}</li>
+                        <li>Distance from Earth: ${json[index].distance}</li>
+                        <li>Number of Moons: ${json[index].moons}</li>
+                     </ol>
+                     <img src=${json[index].image} height=250></img>
+                  </div>
+                  `;
+                  });
+            });
+        });   
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+
+   window.onload=function() {
+
+      let button = document.getElementById("formSubmit");
+      // add event handler for when button clicked
+      button.addEventListener("click", function(event) {
+         event.preventDefault()
+         let pilotName = document.getElementById("pilotName");
+         let copilotName = document.getElementById("copilotName");
+         let fuelLevel = document.getElementById("fuelLevel");
+         let cargoMass = document.getElementById("cargoMass");
+         
+         // now update the value in the input
+         if (!validateName(pilotName.value)) {
+            event.preventDefault()
+            alert("All fields are required!")
+            return
+         }
+         if (!validateName(copilotName.value)) {
+            event.preventDefault()
+            alert("All fields are required!")
+            return
+         }
+         if(!validateNumber(fuelLevel.value) || !validateFuel(fuelLevel.value)) {
+            event.preventDefault()
+            let fuelStatus = document.getElementById("fuelStatus")
+            fuelStatus.innerHTML = "Fuel level too low for launch."
+            let launchStatus = document.getElementById("launchStatus")
+            launchStatus.innerHTML = "Shuttle not ready for launch."
+            let launchStatusCheck = document.getElementById("launchStatusCheck")
+            launchStatusCheck.style.backgroundColor="red"
+            alert("Make sure to enter valid information for each item!")
+            return
+         } 
+      
+         if(!validateNumber(cargoMass.value) || !validateMass(cargoMass.value)) {
+            event.preventDefault()
+            let cargoMass = document.getElementById("cargoStatus")
+            cargoMass.innerHTML = "Too much mass."
+            let launchStatus = document.getElementById("launchStatus")
+            launchStatus.innerHTML = "Shuttle not ready for launch."
+            let launchStatusCheck = document.getElementById("launchStatusCheck")
+            launchStatusCheck.style.backgroundColor="red"
+            alert("Make sure to enter valid information for each item!!!")
+            return
+         } 
+         let pilotStatus = document.getElementById("pilotStatus")
+             pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready for launch`
+          
+         let copilotStatus = document.getElementById("copilotStatus")
+             copilotStatus.innerHTML = `Co-pilot ${copilotName.value} is ready for launch`
+
+ //        let fuelStatus = document.getElementById("fuelStatus")
+  //           fuelStatus.innerHTML = fuelStatus.value  
+
+  //       let cargoStatus = document.getElementById("cargoStatus")
+  //           cargoStatus.innerHTML = cargoStatus.value  
+      });
+      
+   }
+function validateName(name) {
+   if (typeof name !== "string") {
+      return false 
+   }
+   if (name === "" && name.length < 1) {
+      return false
+   }
+   return true;
+}
+function validateNumber(number) {
+   number = parseInt(number)
+   if (isNaN(number)) {
+      return false
+   }
+   return true;
+}
+function validateFuel(value) {
+   if (value > 10000) {
+      return false
+   }
+   return true;
+}
+function validateMass(mass) {
+   if (mass > 10000) {
+      return false
+   }
+   return true;
+}
